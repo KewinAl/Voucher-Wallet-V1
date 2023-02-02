de3e566527d31a44c03496bde069cc9b2f01fd9e
Manuel Ahlert <manuel.ahlert@kamax.com>
2023-02-02 11:07:15 +0100

    - fix user / userprofile / shop profile

--- backend/shopProfile/models.py
+++ backend/shopProfile/models.py
@@ -7,3 +7,5 @@ class ShopProfile(models.Model):
     description = models.CharField(max_length=100, null=True, blank=True)
     link = models.URLField(null=True, blank=True)
     user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="shop_profile")
+    name = models.CharField(max_length=100)
+    REQUIRED_FIELDS = ['__all__']
