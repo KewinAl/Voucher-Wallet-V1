0a4e7ce81d174b7233151cee62afb153fc4515ab
Hitesh Malhotra <hitesh.malhotra@bluewin.ch>
2023-02-08 17:31:03 +0100

    just commits

--- backend/customerProfile/serializers.py
+++ backend/customerProfile/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
-
+from tag.models import Tag
+from tag.serializers import TagSerializer
 from customerProfile.models import CustomerProfile
 
 
 class CustomerProfileSerializer(serializers.ModelSerializer):
     coupon_codes = serializers.StringRelatedField(many=True, read_only=True)
+    preferences = TagSerializer(many=True)
 
     class Meta:
         model = CustomerProfile
-        fields = ['age', 'gender', 'firstname', 'lastname', 'coupon_codes']
+        fields = ['age', 'gender', 'firstname', 'lastname', 'coupon_codes', 'preferences']
--- frontend/src/Components/CustomerProfileCard/index.jsx
+++ frontend/src/Components/CustomerProfileCard/index.jsx
@@ -1,30 +1,30 @@
-const CustomerProfileCard = ({ customerProfileProps }) => {
-  // console.log("Checking Probs", customerProfileProps);
-  return (
-    <>
-      <div className="CustomerProfile">
-        <></>
-        <div className="CustomerUserName">
-          <h3>{"Username: " + customerProfileProps.username}</h3>
-        </div>
-        <div className="Email">
-          {"Email: "}
-          <a href={`mailto:${customerProfileProps.email}`}>
-            {customerProfileProps.email}
-          </a>
-        </div>
-        <div className="CustomerAge">
-          {"Age: " + customerProfileProps.customer_profile.age}
-        </div>
-        <div className="CustomerFirstname">
-          {"Firstname: " + customerProfileProps.customer_profile.firstname}
-        </div>
-        <div className="CustomerLastname">
-          {"Lastname: " + customerProfileProps.customer_profile.lastname}
-        </div>
-      </div>
-    </>
-  );
+const CustomerProfileCard = ({customerProfileProps}) => {
+    // console.log("Checking Probs", customerProfileProps);
+    return (
+        <>
+            <div className="CustomerProfile">
+                <></>
+                <div className="CustomerUserName">
+                    <h3>{"Username: " + customerProfileProps.username}</h3>
+                </div>
+                <div className="Email">
+                    {"Email: "}
+                    <a href={`mailto:${customerProfileProps.email}`}>
+                        {customerProfileProps.email}
+                    </a>
+                </div>
+                <div className="CustomerAge">
+                    {"Age: " + customerProfileProps.customer_profile.age}
+                </div>
+                <div className="CustomerFirstname">
+                    {"Firstname: " + customerProfileProps.customer_profile.firstname}
+                </div>
+                <div className="CustomerLastname">
+                    {"Lastname: " + customerProfileProps.customer_profile.lastname}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default CustomerProfileCard;
