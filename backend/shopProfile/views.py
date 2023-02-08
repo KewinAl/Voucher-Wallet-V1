28585f8ef586c876a8201c6836add54614b9372e
Manuel Ahlert <manuel.ahlert@kamax.com>
2023-02-08 15:08:20 +0100

    - get / patch / delete fpr myShop TODO: ADD permissions !!!

--- backend/shopProfile/views.py
+++ backend/shopProfile/views.py
@@ -1,24 +1,35 @@
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.generics import RetrieveAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import ShopProfile
 from .serializers import ShopProfileSerializer
 
 
-class ShopProfileView(RetrieveUpdateDestroyAPIView):
+class ShopProfileView(RetrieveAPIView):
     queryset = ShopProfile.objects.all()
     serializer_class = ShopProfileSerializer
     lookup_field = 'id'
     lookup_url_kwarg = 'shop_id'  # field in the request
-    http_method_names = ['get', 'post', 'put', 'delete']
-
-
-class LoggedInUserShopProfileView(ListAPIView):
-    serializer_class = ShopProfileSerializer
-
     # permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
+
+class LoggedInUserShopProfileView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ShopProfileSerializer
+    # permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'patch', 'delete']
+
+    def get_object(self):
         input_user_id = self.request.user.id
         queryset = ShopProfile.objects.filter(user=input_user_id)
+        shop_profile = queryset.get()
+        return shop_profile
 
-        return queryset
+# class LoggedInUserShopProfileView(ListAPIView):
+#     serializer_class = ShopProfileSerializer
+#
+#     # permission_classes = [IsAuthenticated]
+#
+#     def get_queryset(self):
+#         input_user_id = self.request.user.id
+#         queryset = ShopProfile.objects.filter(user=input_user_id)
+#
+#         return queryset
