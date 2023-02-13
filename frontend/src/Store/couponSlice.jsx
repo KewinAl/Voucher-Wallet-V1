66a73a2d3d7b02a51957fc6f12c8ddf993e65376
kal <allenspachkewin@gmail.com>
2023-02-13 15:33:16 +0100

    create couponSlice

---
+++ frontend/src/Store/couponSlice.jsx
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const couponSlice = createSlice({
+  name: "coupon",
+  initialState: {
+    coupons: [
+      {
+        id: 1,
+        title: "10% Off on all products",
+        description: "Get 10% discount on all products",
+        times_used: 0,
+        times_redeemed: 0,
+        redeem_limit: 5,
+        discount_type: "percent",
+        discount: 10,
+        currency: "USD",
+        expiration_date: "2023-06-30",
+        shop_profile: {
+          id: 1,
+          name: "Shop A",
+        },
+        tag: {
+          id: 1,
+          name: "Summer Sale",
+        },
+      },
+      {
+        id: 2,
+        title: "$5 Off on first purchase",
+        description: "Get $5 discount on your first purchase",
+        times_used: 0,
+        times_redeemed: 0,
+        redeem_limit: 1,
+        discount_type: "amount",
+        discount: 5,
+        currency: "USD",
+        expiration_date: "2023-12-31",
+        shop_profile: {
+          id: 2,
+          name: "Shop B",
+        },
+        tag: {
+          id: 2,
+          name: "Special Limited Edition",
+        },
+      },
+    ],
+  },
+  reducers: {
+    addCoupon: (state, action) => {
+      state.coupons.push(action.payload);
+    },
+  },
+});
+
+export const { addCoupon } = couponSlice.actions;
+
+export default couponSlice.reducer;
--- frontend/src/Store/rootStore.jsx
+++ frontend/src/Store/rootStore.jsx
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import tagReducer from "./tagSlice";
 import customerReducer from "./customerSlice";
+import couponReducer from "./couponSlice";
 
 const rootStore = configureStore({
-    reducer: {
-        customer: customerReducer,
-        auth: authReducer, //exporting directly as reducer
-        tags: tagReducer //exporting directly as reducer
-        }
-})
+  reducer: {
+    customer: customerReducer,
+    auth: authReducer, //exporting directly as reducer
+    tags: tagReducer, //exporting directly as reducer
+    coupons: couponReducer,
+  },
+});
 
-export default rootStore
+export default rootStore;
