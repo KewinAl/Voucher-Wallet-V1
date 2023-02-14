50120451dab3dfbee4fa6311a0ca1b00a8ab1ce5
Manuel Ahlert <manuel.ahlert@kamax.com>
2023-02-14 12:40:08 +0100

    - Redeem Coupon QR Code

---
+++ frontend/src/Pages/CustomerProfile/ShowCoupon/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import {
+  Amount,
+  CodeSection,
+  CompanyLogo,
+  CompanyName,
+  CouponContainer,
+  CouponTitle,
+  Description,
+  Expiration,
+  LeftContainer,
+  OurLogo,
+  RedeemButton,
+  RightContainer,
+} from "./coupon.styles";
+import logo from "../../../Assets/voucherwallet-transparent.png";
+import RedeemOverlay from "../RedeemOverlay/overlay";
+
+const Coupon = ({ details }) => {
+  const [overlayVisibility, setOverlayVisibility] = useState(false);
+
+  return (
+    <>
+      <CouponContainer>
+        <RedeemOverlay
+          visible={overlayVisibility}
+          exitFunction={setOverlayVisibility}
+        />
+        <LeftContainer>
+          <Amount>
+            35%
+            <br />
+            OFF
+          </Amount>
+          <Expiration>
+            Expiration:
+            <code>{details.expiration_date}</code>
+          </Expiration>
+          <CompanyName>{details.company}</CompanyName>
+          <OurLogo src={logo} />
+          <CompanyLogo src={details.companyLogo} />
+        </LeftContainer>
+        <RightContainer>
+          <CouponTitle>{details.title}</CouponTitle>
+          <CodeSection>
+            <label>Voucher Code</label>
+            <code>XXX-XXXX</code>
+          </CodeSection>
+          <Description>
+            {details.description}
+            <small>*Terms and Conditions apply.</small>
+            <RedeemButton
+              onClick={() => setOverlayVisibility(!overlayVisibility)}
+            >
+              Redeem
+            </RedeemButton>
+            <a href={details.link}>{details.link}</a>
+          </Description>
+        </RightContainer>
+      </CouponContainer>
+    </>
+  );
+};
+
+export default Coupon;
--- frontend/src/Pages/CustomerProfile/index.js
+++ frontend/src/Pages/CustomerProfile/index.js
@@ -1,78 +1,77 @@
 import {
-    CouponList,
-    CustomerNavigation,
-    CustomerProfileContainerPage,
-    NavigationContent,
+  CouponList,
+  CustomerNavigation,
+  CustomerProfileContainerPage,
+  NavigationContent,
 } from "./CustomerProfile.styles";
-import {useDispatch, useSelector} from "react-redux";
-import Coupon from "../../Components/CouponCard/Coupon";
+import { useDispatch, useSelector } from "react-redux";
+import Coupon from "./ShowCoupon";
 
 const CustomerProfile = () => {
-    const dispatch = useDispatch();
-    const token = useSelector((store) => store.auth.access);
-    const store = useSelector((store) => store);
-    const first_name = useSelector((store) => store.customer.firstname);
-    const last_name = useSelector((store) => store.customer.lastname);
-    const gender = useSelector((store) => store.customer.gender);
-    let coupons = useSelector((store) => store.coupons.coupons); //hardcoded
-    // console.log("COUPONS:", coupons);
-    // console.log("COUPONS 0:", coupons.map(coupon));
+  const dispatch = useDispatch();
+  const token = useSelector((store) => store.auth.access);
+  const store = useSelector((store) => store);
+  const first_name = useSelector((store) => store.customer.firstname);
+  const last_name = useSelector((store) => store.customer.lastname);
+  const gender = useSelector((store) => store.customer.gender);
+  let coupons = useSelector((store) => store.coupons.coupons); //hardcoded
+  // console.log("COUPONS:", coupons);
+  // console.log("COUPONS 0:", coupons.map(coupon));
 
-    console.log("store", store);
-    console.log("couponsOnly", coupons);
-    // useEffect(() => {
-    //   fetchProfile();
-    // }, []);
+  console.log("store", store);
+  console.log("couponsOnly", coupons);
+  // useEffect(() => {
+  //   fetchProfile();
+  // }, []);
 
-    // const fetchProfile = () => {
-    //   var config = {
-    //     method: "get",
-    //     maxBodyLength: Infinity,
-    //     url: "https://voucher-wallet.propulsion-learn.ch/backend/api/customer/2/",
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //     data: "",
-    //   };
+  // const fetchProfile = () => {
+  //   var config = {
+  //     method: "get",
+  //     maxBodyLength: Infinity,
+  //     url: "https://voucher-wallet.propulsion-learn.ch/backend/api/customer/2/",
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //     data: "",
+  //   };
 
-    //   axios(config)
-    //     .then(function (response) {
-    //       console.log(response.data);
-    //       dispatch(setCustomerDetails(response.data));
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //     });
-    // };
-    if (!Array.isArray(coupons)) {
-        coupons = [coupons];
-    }
+  //   axios(config)
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //       dispatch(setCustomerDetails(response.data));
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
+  if (!Array.isArray(coupons)) {
+    coupons = [coupons];
+  }
 
-    return (
-        <>
-            <CustomerProfileContainerPage>
-                <CustomerNavigation>
-                    <h1>Customer Profile</h1>
-                    <p>
-                        {first_name} {last_name}
-                    </p>
-                    <button>Preferences</button>
-                    <button>Notifications Coupons</button>
-                    <button>Edit Profile</button>
-                    <button>Edit Profile</button>
-                    <button>Search All Coupons</button>
-                </CustomerNavigation>
-                <NavigationContent>
-                    <CouponList>
-                        {coupons.map((coupon) => (
-                            <Coupon details={coupon}/>
-                        ))}
-
-                    </CouponList>
-                </NavigationContent>
-            </CustomerProfileContainerPage>
-        </>
-    );
+  return (
+    <>
+      <CustomerProfileContainerPage>
+        <CustomerNavigation>
+          <h1>Customer Profile</h1>
+          <p>
+            {first_name} {last_name}
+          </p>
+          <button>Preferences</button>
+          <button>Notifications Coupons</button>
+          <button>Edit Profile</button>
+          <button>Edit Profile</button>
+          <button>Search All Coupons</button>
+        </CustomerNavigation>
+        <NavigationContent>
+          <CouponList>
+            {coupons.map((coupon) => (
+              <Coupon details={coupon} />
+            ))}
+          </CouponList>
+        </NavigationContent>
+      </CustomerProfileContainerPage>
+    </>
+  );
 };
 
 export default CustomerProfile;
