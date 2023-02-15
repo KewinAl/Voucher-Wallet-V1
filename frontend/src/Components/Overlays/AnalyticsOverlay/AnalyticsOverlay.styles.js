a1e1ac6e89525b93d2f34f177f33ab45f467aa4b
Hitesh Malhotra <hitesh.malhotra@bluewin.ch>
2023-02-15 17:04:35 +0100

    some styling on analytics

--- frontend/src/Components/Overlays/AnalyticsOverlay/AnalyticsOverlay.styles.js
+++ frontend/src/Components/Overlays/AnalyticsOverlay/AnalyticsOverlay.styles.js
@@ -1,76 +1,96 @@
 import styled from "styled-components";
 
 export const AnalyticsOverlayDiv = styled.div`
-        width: 900px;
-        height: 600px;
-        background-color: white;
-        border: 1px solid #cccccc;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-around;
-        padding: 20px;
-        h1 {
-            font-size: x-large;
-        }
-        h2 {
-            font-size: x-large;
-            text-align: center;7
-        }
-    `
+  width: 900px;
+  height: 600px;
+  background-color: white;
+  border: 1px solid #cccccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+
+  h1 {
+    font-size: x-large;
+    color: blueviolet;
+
+  }
+
+  h2 {
+    font-size: x-large;
+    text-align: center;
+    color: blueviolet;
+    border: 2px dotted orange;
+    padding: 10px;
+
+  }
+`
 
 export const AnalyticsDiv = styled.div`
-    width: 70%;
-    height: 100%;
+  width: 70%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  > div {
+    width: 100;
     display: flex;
-    flex-direction: column; 
-    justify-content: space-around;
-    >div {
-        width: 100;
-        display: flex;
-        justify-content: space-between;
+    justify-content: space-between;
+    color: darkgoldenrod;
+
+  }
+
+
+  > div.CouponsRedeemed {
+    margin-top: 50px;
+    background: lightblue;
+    border-radius: 25px;
+    padding: 15px;
+  }
+
+  > div.UserAnalytics {
+    background-color: #d8f8d8;
+    height: 50%;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-items: space-between;
+    border-radius: 15px;
+
+
+    > div {
+      display: flex;
+      justify-content: space-between;
     }
-    >div.CouponsRedeemed {
-        margin-top: 50px;
-        background: lightblue;
-        border-radius: 25px;
-        padding: 15px;
-    }
-    >div.UserAnalytics {
-        background-color: lightgray;
-        height: 50%;
-        padding: 15px;
-        display: flex;
-        flex-direction: column;
-        justify-items: space-between;
-        border-radius: 15px;
-        >div{
-            display: flex;
-            justify-content: space-between;
-        }
-    }
+  }
 `
 
 export const ResultsDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+
+  > div {
     display: flex;
-    justify-content: space-around;
-    width: 50%;
-    >div {
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-    }
+    flex-direction: column;
+    text-align: center;
+  }
 `
 
 export const LocationList = styled.div`
-width: 100%;
-height: 100px;
-overflow-y: auto;
+  width: 100%;
+  height: 100px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+
+
+  > div {
+    width: 100%;
     display: flex;
-    flex-direction: column; 
-    >div{
-        width: 100%;
-        display: flex;
-        justify-content: space-around;
-    }
+    justify-content: space-around;
+
+  }
 `
