1cc6de01b7552b9e8a5b866eb37cbe971acb164b
kal <allenspachkewin@gmail.com>
2023-02-15 16:23:06 +0100

    add persistent store

--- frontend/src/Store/rootStore.jsx
+++ frontend/src/Store/rootStore.jsx
@@ -1,16 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
 import authReducer from "./authSlice";
 import tagReducer from "./tagSlice";
 import customerReducer from "./customerSlice";
 import couponReducer from "./couponSlice";
 
-const rootStore = configureStore({
-  reducer: {
-    customer: customerReducer,
-    auth: authReducer, //exporting directly as reducer
-    tags: tagReducer, //exporting directly as reducer
-    coupons: couponReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "tags", "coupons"],
+};
+
+const rootReducer = combineReducers({
+  customer: customerReducer,
+  auth: authReducer,
+  tags: tagReducer,
+  coupons: couponReducer,
 });
 
-export default rootStore;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const rootStore = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(rootStore);
+
+export { rootStore, persistor };
