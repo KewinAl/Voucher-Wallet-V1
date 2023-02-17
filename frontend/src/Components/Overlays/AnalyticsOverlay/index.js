eb8caf7addbdfdbfe320b440be31fc962a139846
Manuel Ahlert <manuel.ahlert@kamax.com>
2023-02-17 13:53:52 +0100

    - added some styling to analytics

--- frontend/src/Components/Overlays/AnalyticsOverlay/index.js
+++ frontend/src/Components/Overlays/AnalyticsOverlay/index.js
@@ -1,9 +1,11 @@
+import React from "react";
 import {
   AnalyticsDiv,
   AnalyticsOverlayDiv,
   LocationList,
   ResultsDiv,
 } from "./AnalyticsOverlay.styles";
+import { Cell, Pie, PieChart, Tooltip } from "recharts";
 
 const AnalyticsOverlay = ({
   visible,
@@ -18,17 +20,48 @@ const AnalyticsOverlay = ({
     }
   };
   let percentage = (redeemed / redeemable) * 100;
+
+  const malePercent = analytics.male;
+  const femalePercent = analytics.female;
+  const otherPercent = analytics.other;
+
+  const colors = ["#06c076", "#522fbc", "#f5a623"];
+
+  const data = [
+    {
+      y: malePercent,
+      color: colors[0],
+      label: "Male",
+    },
+    {
+      y: femalePercent,
+      color: colors[1],
+      label: "Female",
+    },
+    {
+      y: otherPercent,
+      color: colors[2],
+      label: "Other",
+    },
+  ];
+
+  // Sort data array in descending order of y values
+  // data.sort((a, b) => b.y - a.y);
+
   return (
     <AnalyticsOverlayDiv>
-      <h1>Analytics</h1>
+      <h1>Coupon Analytics</h1>
       <AnalyticsDiv>
         <div className="CouponsRedeemed">
           <span>Coupons redeemed:</span>
-          <ResultsDiv>
-            <span>{percentage}%</span>
-            <span>
-              {redeemed}/{redeemable}
-            </span>
+          <span>
+            ({redeemed} of {redeemable})
+          </span>
+          <ResultsDiv percentage={percentage}>
+            <span className="Percentage">{percentage}%</span>
+            {/*<span className="Redeemed">*/}
+            {/*  ({redeemed} / {redeemable})*/}
+            {/*</span>*/}
           </ResultsDiv>
         </div>
         <h2>User Analytics</h2>
@@ -39,20 +72,38 @@ const AnalyticsOverlay = ({
               <span>{analytics.averageAge}</span>
             </ResultsDiv>
           </div>
-          <div>
+          <div className="GenderAnalytics">
             <span>Gender</span>
             <ResultsDiv>
-              <div>
-                <span>{analytics.male}%</span>
-                <span>male</span>
+              <div className="PieChart">
+                <PieChart width={150} height={150}>
+                  <Pie
+                    data={data}
+                    dataKey="y"
+                    nameKey="label"
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={0}
+                    outerRadius={60}
+                    fill="#8884d8"
+                  >
+                    {data.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={colors[index % colors.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip />
+                </PieChart>
               </div>
-              <div>
-                <span>{analytics.female}%</span>
-                <span>female</span>
-              </div>
-              <div>
-                <span>{analytics.other}%</span>
-                <span>other</span>
+              <div className="GenderLegend">
+                <div className="GenderLegendBox MaleSlice"></div>
+                <span>{malePercent}% male</span>
+                <div className="GenderLegendBox FemaleSlice"></div>
+                <span>{femalePercent}% female</span>
+                <div className="GenderLegendBox OtherSlice"></div>
+                <span>{otherPercent}% other</span>
               </div>
             </ResultsDiv>
           </div>
