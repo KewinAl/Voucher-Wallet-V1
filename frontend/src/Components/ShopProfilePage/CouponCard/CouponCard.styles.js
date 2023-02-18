46024cacf8291bd30a074010c1aecd2a89dbc636
swapnil umrikar <neha29.shrikant@gmail.com>
2023-02-18 11:02:39 +0100

    pulled from master

--- frontend/src/Components/ShopProfilePage/CouponCard/CouponCard.styles.js
+++ frontend/src/Components/ShopProfilePage/CouponCard/CouponCard.styles.js
@@ -2,83 +2,88 @@ import styled from "styled-components";
 
 
 export const CouponDiv = styled.div`
-    padding: 20px 5px 0px;
-    height: 24rem;
-    width: 16rem;
-    box-shadow: 5px 7px 14px -3px rgba(0,0,0,0.38);
-    background: white;
-    border: 3px solid #cccccc;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    h1 {
-        font-size: large;
-        text-decoration: underline;
-        font-weight: 600;
-    }
-    >p {
-        max-height: 50%;
-        width: 80%;
-        overflow: auto;
-    }
+  padding: 20px 5px 0px;
+  height: 24rem;
+  width: 16rem;
+  box-shadow: 5px 7px 14px -3px rgba(0, 0, 0, 0.38);
+  background: white;
+  border: 3px solid #cccccc;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: large;
+    text-decoration: underline;
+    font-weight: 600;
+  }
+
+  > p {
+    max-height: 50%;
+    width: 80%;
+    overflow: auto;
+  }
 `;
 
 export const Tag = styled.div`
-    background-color: ${p => p.theme.backgroundPurpleLight};
-    border-radius: 15px;
-    color: white;
-    text-align: center;
-    margin: 5px 0px;
-    padding: 5px 15px ;
+  background-color: ${p => p.theme.backgroundPurpleLight};
+  border-radius: 15px;
+  color: white;
+  text-align: center;
+  margin: 5px 0px;
+  padding: 5px 15px;
 `
 export const DetailsDiv = styled.div`
-    width: 80%;
-    padding: 15px 0px;
-    display: flex;
-    justify-content: space-between;
-    p {
-        font-size: small;
-        color: #707070;
-    }
-    .aligned-end {
-        text-align: end;
-    }
+  width: 80%;
+  padding: 15px 0px;
+  display: flex;
+  justify-content: space-between;
+
+  p {
+    font-size: small;
+    color: #707070;
+  }
+
+  .aligned-end {
+    text-align: end;
+  }
 `
 
 export const CouponDescription = styled.p`
-    height: 30%;
-    border-radius: 5px;
-    padding: 5px;
-    overflow: auto;
-    text-align: center;
+  height: 30%;
+  border-radius: 5px;
+  padding: 5px;
+  overflow: auto;
+  text-align: center;
 `
 
 export const CouponButtonDiv = styled.div`
-    width: 90%;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  margin: auto 0px 20px;
+
+  > div {
     display: flex;
-    flex-direction: column;
-    gap:5px;
-    margin: auto 0px 20px;
-    >div {
-        display: flex;
-    }
+  }
 `
 
 export const AnalyticsButton = styled.button`
-    width: 100%;
-    height: 45px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 10px;
-    gap: 10px;
-    border: solid 2px ${p => p.theme.backgroundPurpleLight};
-    border-radius: 5px;
-    color: ${p => p.theme.backgroundPurpleLight};
-    background: none;
-    
-    &:hover {
+  width: 100%;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  gap: 10px;
+  border: solid 2px ${p => p.theme.backgroundPurpleLight};
+  border-radius: 5px;
+  color: ${p => p.theme.backgroundPurpleLight};
+  background: none;
+
+  &:hover {
     border: transparent solid 2px;
     background: ${p => p.theme.backgroundPurpleLight};
     cursor: pointer;
@@ -86,19 +91,19 @@ export const AnalyticsButton = styled.button`
 `
 
 export const CouponActionButton = styled.div`
-    width: 50%;
-    height: 45px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 10px;
-    gap: 10px;
-    border: solid 2px ${props => props.color};
-    border-radius: 5px;
-    color: ${props => props.color};
-    background: none;
-    
-    &:hover {
+  width: 50%;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  gap: 10px;
+  border: solid 2px ${props => props.color};
+  border-radius: 5px;
+  color: ${props => props.color};
+  background: none;
+
+  &:hover {
     border: transparent solid 2px;
     background: ${props => props.color};
     cursor: pointer;
--- frontend/src/Components/ShopProfilePage/ShopProfile.styles.js
+++ frontend/src/Components/ShopProfilePage/ShopProfile.styles.js
@@ -2,90 +2,92 @@ import styled from "styled-components";
 
 
 export const PageDiv = styled.div`
-    max-height: 90%;
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    align-items: center;
+  max-height: 90%;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
 
-    >p {
-        font-size: xx-large;
-        padding: 10px 0px;
-    }
+  > p {
+    font-size: xx-large;
+    padding: 10px 0px;
+  }
 `;
 export const Menu = styled.div`
-    display: flex;
-    flex-direction: column; 
-    width: 90%;
-    max-height: 90%;
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  max-height: 90%;
 `
 export const MenuLeft = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding-bottom: 15px;
-    gap: 25px;
-    >button {
-        border-radius: 10px;
-    }
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 15px;
+  gap: 25px;
+
+  > button {
+    border-radius: 10px;
+  }
 `
 export const NewCouponButton = styled.button`
-    width: 30%;
-    height: 45px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 10px;
-    gap: 10px;
-    border: solid 2px ${p => p.theme.green};
-    border-radius: 5px;
-    color: ${p => p.theme.green};
-    background: none;
-    
-    &:hover {
+  width: 30%;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  gap: 10px;
+  border: solid 2px ${p => p.theme.green};
+  border-radius: 5px;
+  color: ${p => p.theme.green};
+
+
+  &:hover {
     border: transparent solid 2px;
     background: ${p => p.theme.green};
     cursor: pointer;
     color: white;
 `
 export const MenuRight = styled.div`
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    // border-radius: 0px 30px 30px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  // border-radius: 0px 30px 30px;
 
-    >div{
+  > div {
     width: 100%;
     height: 100%;
     min-height: 800px;
     // border: 2px solid #cccccc;
     padding: 15px 0px;
     box-sizing: border-box;
-    // background-color: #e6e6e6;
+    //background-color: #e6e6e6;
     // box-shadow: inset 7px 5px 16px -5px rgba(0,0,0,0.44);
     display: flex;
     flex-wrap: wrap;
     gap: 30px;
     overflow-y: auto;
 
+
     ::-webkit-scrollbar {
-	-webkit-box-shadow: none;
-	border-radius: 10px;
-	background-color: none;
-    width: 10px;
+      -webkit-box-shadow: none;
+      border-radius: 10px;
+      width: 10px;
     }
 
-    ::-webkit-scrollbar-thumb{
-	border-radius: 10px;
-	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-	background-color: #cccccc;
+    ::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+      background-color: #cccccc;
     }
 
     ::-webkit-scrollbar-button:end:increment {
-    height: 3%;
+      height: 3%;
     }
+
     ::-webkit-scrollbar-button:start:decrement {
-    height: 3%;
+      height: 3%;
     }
-    }
+  }
 `
